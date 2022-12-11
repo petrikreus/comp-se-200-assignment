29f89aa696417b403a87d20f437b90d8d55ce988
Petri Kreus <anxored@gmail.com>
2022-12-11 13:40:12 +0200

    Update tests for words.js to conform with the new understanding of the function

--- tests/words.test.js
+++ tests/words.test.js
@@ -2,11 +2,30 @@ import words from 'software-testing-assignment/src/words'
 
 describe('words.js', () => {
 
-  test('Input string is divided into individual search words', () => {
+  test('Returns an array of individual words', () => {
     const inputString = 'one two three'
-    const delimiter = ' '
     const expectedResult = ['one', 'two', 'three']
-    expect(words(inputString, delimiter)).toEqual(expect.arrayContaining(expectedResult))
+    expect(words(inputString)).toEqual(expectedResult)
+  })
+
+  test('Returns an array containing the first found match, based on given matcher string', () => {
+    const inputString = 'one two three'
+    const matcher = 'two'
+    const expectedResult = ['two']
+    expect(words(inputString, matcher)).toEqual(expectedResult)
+  })
+
+  test('Returns an array of individual words, based on given global regexp', () => {
+    const inputString = 'one, two, & three'
+    const regexp = /[^, ]+/g // Negated set: match any character not included in the set
+    const expectedResult = ['one', 'two', '&', 'three']
+    expect(words(inputString, regexp)).toEqual(expectedResult)
+  })
+
+  test('Returns an empty array if no matches are found', () => {
+    const inputString = 'one two three'
+    const matcher = 'four'
+    expect(words(inputString, matcher)).toEqual([])
   })
 
 })
